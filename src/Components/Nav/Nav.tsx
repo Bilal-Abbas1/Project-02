42a278c7553409f7e1bd5b00ade81d9f14cad989
Bilal-Abbas1 <muhammedbilalabbas229@gmail.com>
2024-01-24 01:34:34 +0500

    first commit

---
+++ src/Components/Nav/Nav.tsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import Image from 'next/image'
+import Link from 'next/link'
+const Nav = () => {
+  return (
+    <div>
+      <header className="flex justify-between gap-5 py-2 px-2 bg-blue-300 text-center text-2xl  text-black-800 items-center">
+    
+    <ul className='flex space-x-8 text-red-500'>
+    <Link href="\">Home</Link>
+    <Link href="\Name">Name</Link>
+    <Link href="\Name\Address">Address</Link>
+    <Link href="\Name\Address\contact">Contact Us</Link>
+    </ul>
+    </header>
+    </div>
+
+  )
+}
+
+export default Nav
